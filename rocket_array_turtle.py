5e684be774a97854be6580ba146ccf7b189f28dd
sruiz <sruiz@ankh>
2009-06-09 18:36:13 -0400

    made server.py executable refined rocket_array.py - added some functions to make it easier to auto-populate arrays TODO - add comments, docstrings tiny bit more work on rocket_array_turtle.py

--- rocket_array_turtle.py
+++ rocket_array_turtle.py
@@ -3,21 +3,30 @@ from rocket_array import *
 import turtle
 
 screen = turtle.Screen()
-screen.setup(width = 0.5, height = 0.5)
 screen.mode("logo")
+screen.setup(width = 0.5, height = 0.5)
+screen.title("Turtle Rockets!")
 screen.setworldcoordinates(-4,-4,4,4)
 
+def toLogo(heading):
+    if heading > 90:
+        return 360 - (heading - 90)
+    return 90 - heading
 
-class TurtleRocket(Rocket):
+class TurtleRocket(ServerRocket):
     '''Turtle rockets. Barely started...'''
-    def __init__(self, (x,y), heading = 0, *args, **kwargs):
+    def __init__(self, server, rocket_id,(x,y), heading = 0, *args, **kwargs):
         self.turtle = turtle.Turtle()
+        super(TurtleRocket, self).__init__(server, rocket_id,(x,y),
+                                           heading = heading, *args, **kwargs)
+        self.turtle.shape("arrow")
+        self.turtle.penup()
         self.turtle.goto(x,y)
-        super(TurtleRocket, self).__init__((x,y), heading, *args, **kwargs)
+        self.turtle.setheading(toLogo(heading))
 
     #def turn(self,degrees, debug = True):
     #    self.server.command(self.rocket_id,"turn:%s" % degrees)
     #    if debug:
     #        super(ServerRocket, self).turn(degrees)
-
-rs = RocketArray([TurtleRocket((i,0)) for i in range(-2,3)])
+    
+rs = server_array(rocket_server(), ((-2,-2),(0,2),(2,-2)) , rocket_type = TurtleRocket, recenter = False)
